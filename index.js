74c2e2ff834abdea0f4ac70021545c90d1758842
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2024-01-03 22:36:32 -0700

    Add button and change body flex direction to accomodate button

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-///need to generate columns and rows in loop
-
-//grab container element
-//grab number for grid
-//loop # times
-    //create row container
-    //loop # times
-        //create tile
-        //add tile to row container
-    //add row container to container
-//add container to document.body
-
 let container = document.querySelector('.container');
+
+let btn = document.createElement('button');
+btn.innerText = 'Change Grid Size';
+document.body.insertBefore(btn, container);
 
 for (let i = 0; i < 16; i++) {
     row = document.createElement('div');
